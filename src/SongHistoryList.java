d57e7809c7c8581bdd3a53d248e4f68c665e92fa
Jack Shkifati <97077461+JackShkifati28@users.noreply.github.com>
2023-06-08 14:36:13 -0400

    Add files via upload

---
+++ src/SongHistoryList.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vipclients;
+
+public class SongHistoryList {
+
+    private Song first;
+
+    public void SongHistoryList() {
+        // constructor for creating a new list
+    }
+
+    public void addSong(Song s) {
+
+    }
+
+//    public Song lastListened() {
+//        // retrieves the next song to listen to
+//    }
+
+}
